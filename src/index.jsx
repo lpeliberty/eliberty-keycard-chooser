8eeeae5a30c41d5ad9b68df8444d17ee435ff439
ymeliberty <yeimi.mejias@eliberty.fr>
2017-10-06 10:25:48 +0200

    First commit

--- src/index.jsx
+++ src/index.jsx
@@ -4,4 +4,10 @@ import KeycardChooser from './components/KeycardChooser';
 import '../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss';
 import '../node_modules/bootstrap-sass/assets/javascripts/bootstrap';
 
-render(<KeycardChooser name="React Lib keycard-chooser" />, document.getElementById('root'));
+//render(<KeycardChooser name="React Lib keycard-chooser" />, document.getElementById('root'));
+
+const ElibKeycardChooser = (props) => (
+  <KeycardChooser {...props} />
+);
+
+export default ElibKeycardChooser;
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,20 +1,73 @@
-const config = require('./webpack.config.js');
 const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-config.plugins.push(
-  new webpack.DefinePlugin({
-    "process.env": {
-      "NODE_ENV": JSON.stringify("production")
-    }
-  })
-);
+const extractSass = new ExtractTextPlugin({
+  filename: 'dist/main.css',
+});
 
-config.plugins.push(
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: false
-    }
-  })
-);
+module.exports = {
+  entry: [
+    './src/index'
+  ],
+  module: {
+    loaders: [
+      {
+        test: /bootstrap.+\.(jsx|js)$/,
+        loader: 'imports-loader?jQuery=jquery,$=jquery,this=>window',
+      },
+      {
+        test: /\.s[ca]ss$/,
+        use: extractSass.extract({
+          use: [{
+            loader: 'css-loader',
+          }, {
+            loader: 'sass-loader',
+          }],
+          fallback: 'style-loader',
+        }),
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components|legacyjs)/,
+        loaders: ['babel-loader'],
+      },
+      {
+        test: /\.*.svg$/,
+        loader: 'svg-inline-loader',
+      },
+      { test: /\.(woff|woff2|eot|ttf|otf)$/, loader: 'file-loader?limit=100000' }
+    ],
 
-module.exports = config;
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules: [__dirname, 'app', 'node_modules'],
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devtool: 'cheap-eval-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        "NODE_ENV": JSON.stringify("production")
+      }
+    }),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compress: {
+    //     warnings: false
+    //   }
+    // }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    extractSass
+  ]
+};
