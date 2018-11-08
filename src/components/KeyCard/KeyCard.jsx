464d6612c6e3ae5b99448e5ca988617d7fd847c5
ymeliberty <yeimi.mejias@eliberty.fr>
2018-11-08 17:51:34 +0100

    add function for initialize zipcode

--- src/components/KeyCard/KeyCard.jsx
+++ src/components/KeyCard/KeyCard.jsx
@@ -74,7 +74,7 @@ class KeyCard extends React.Component {
   handleChangeCheckSwisspass() {
     const currentId = this.props.localItemInfo.get('skierIndex');
     const currentItem = this.props.localItemInfo.get(currentId);
-    const newValue = !currentItem.get('swisspassElem').get('checked');
+    const newValue = !currentItem.get('swisspassElem', new Map()).get('checked');
 
     this.props.updateSwissPassElem(currentId, 'checked', newValue);
   }
@@ -85,7 +85,7 @@ class KeyCard extends React.Component {
    */
   handleChangeZipcode(event) {
     const currentId = this.props.localItemInfo.get('skierIndex');
-    const newValue = !this.verifyZipcode(event.target.value);
+    const newValue = !KeyCard.verifyZipcode(event.target.value);
     this.props.updateSwissPassElem(currentId, 'zipcode', event.target.value);
     this.props.updateSwissPassElem(currentId, 'validZipcode', newValue);
   }
