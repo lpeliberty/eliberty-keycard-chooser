54dbb2e856688e859b44c93558843c8dee4db8b3
lpeliberty <lpeliberty@eliberty.fr>
2019-04-05 14:48:57 +0200

    toto test

--- src/components/KeyCard/KeyCard.jsx
+++ src/components/KeyCard/KeyCard.jsx
@@ -448,7 +448,7 @@ class KeyCard extends React.Component {
 
     return (
       <div className="blockPopover" key={id}>
-
+        <div>TOTO TEST</div>
         <div className="col-xs-4 keyCardAreaImage">
           <img src={keycardPictureSrc} alt="keycardPicture" />
         </div>
